263028ec01e6eca8a17c33be376b7b39b324aaa4
JavierNila <javiernila66@aragon.unam.mx>
2025-03-10 22:27:32 -0400

    DOCS: se agregaron comentarios

--- patch.py
+++ patch.py
@@ -2,9 +2,11 @@ import random
 from celda import Celda
 from nido import Nido
 
+# Clase que representa el entorno donde se mueven las hormigas
 class Patch:
     def __init__(self, ancho, alto):
-        self.ancho = ancho
-        self.alto = alto
-        self.ambiente = [[Celda() for _ in range(ancho)] for _ in range(alto)]
-        self.nidos = {}
+        self.ancho = ancho  # Ancho del entorno
+        self.alto = alto  # Alto del entorno
+        self.ambiente = [[Celda() for _ in range(ancho)] for _ in range(alto)]  # Matriz de celdas que conforma el entorno
+        self.nidos = {}  # Diccionario para almacenar los nidos de cada colonia
+
