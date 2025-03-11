0573d0e0710fa45be6a62c013783f60e80f256e3
JavierNila <javiernila66@aragon.unam.mx>
2025-03-10 22:55:47 -0400

    FEAT: Se define la funcion agregar_flores

--- patch.py
+++ patch.py
@@ -10,3 +10,12 @@ class Patch:
         self.ambiente = [[Celda() for _ in range(ancho)] for _ in range(alto)]  # Matriz de celdas que conforma el entorno
         self.nidos = {}  # Diccionario para almacenar los nidos de cada colonia
 
+    def agregar_flores(self, num_flores, energia_flor):
+        for _ in range(num_flores):
+            x, y = random.randint(0, self.ancho - 1), random.randint(0, self.alto - 1)
+            celda = self.ambiente[y][x]
+            
+            if celda.nido is None and celda.estado == 3:
+                celda.estado = 0
+                celda.energia = energia_flor  
+
