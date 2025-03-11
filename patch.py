91e42a5dde60f303b98be142dd2fd28daafe0c8e
JavierNila <javiernila66@aragon.unam.mx>
2025-03-10 23:01:35 -0400

    DOCS: Se agregan comentarios a la funcion creada

--- patch.py
+++ patch.py
@@ -10,12 +10,15 @@ class Patch:
         self.ambiente = [[Celda() for _ in range(ancho)] for _ in range(alto)]  # Matriz de celdas que conforma el entorno
         self.nidos = {}  # Diccionario para almacenar los nidos de cada colonia
 
+    # Método para agregar flores en posiciones aleatorias del entorno
     def agregar_flores(self, num_flores, energia_flor):
         for _ in range(num_flores):
+            # Generar coordenadas aleatorias dentro del entorno
             x, y = random.randint(0, self.ancho - 1), random.randint(0, self.alto - 1)
-            celda = self.ambiente[y][x]
+            celda = self.ambiente[y][x]  # Obtener la celda en la posición generada
             
+            # Verificar que la celda no contenga un nido y que sea un espacio vacío
             if celda.nido is None and celda.estado == 3:
-                celda.estado = 0
-                celda.energia = energia_flor  
+                celda.estado = 0  # Cambiar estado a flor
+                celda.energia = energia_flor  # Asignar la energía de la flor
 
