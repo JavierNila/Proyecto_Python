cf66d8de130fbc589954f74b74f37b1a04fc4867
JavierNila <javiernila66@aragon.unam.mx>
2025-03-12 18:48:19 -0400

    FEAT: Se cambio el tamaño del ambiente y posicion de nidos

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from patch import Patch
 
 if __name__ == "__main__":
-    # Crear un ambiente de 20x20
-    ambiente = Patch(20, 20)
+    # Crear un ambiente de 30x15
+    ambiente = Patch(30, 15)
 
     # Agregar 40 flores con 50 de energía cada una
-    ambiente.agregar_flores(35, 50)
+    ambiente.agregar_flores(40, 50)
 
     # Colocar dos nidos en posiciones específicas
-    ambiente.colocar_nido(15, 5, 'A')
-    ambiente.colocar_nido(3, 15, 'B')
+    ambiente.colocar_nido(25, 3, 'A')
+    ambiente.colocar_nido(4, 11, 'B')
 
     # Mostrar el estado del ambiente sin hormigas
     ambiente.mostrar_estado([])
