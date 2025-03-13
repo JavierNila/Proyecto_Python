49825ab55b8096b42d210f77ef67d1bfd1cdf112
JavierNila <javiernila66@aragon.unam.mx>
2025-03-13 09:41:30 -0400

    FEAT: Se termina de definir la funcion buscar_comida

--- hormiga.py
+++ hormiga.py
@@ -16,3 +16,14 @@ class Hormiga:
         x, y = self.posicion
         dx, dy = random.choice([(0, 1), (1, 0), (0, -1), (-1, 0)])
         nueva_x, nueva_y = x + dx, y + dy
+        if 0 <= nueva_x < self.patch.ancho and 0 <= nueva_y < self.patch.alto:
+            nueva_celda = self.patch.ambiente[nueva_y][nueva_x]
+            if nueva_celda.estado == 0:
+                nueva_celda.estado = 3
+                nueva_celda.energia = 0
+                self.tiene_comida = True
+                self.energia += 20
+            self.posicion = (nueva_x, nueva_y)
+        self.energia -= 1
+        if self.energia <= 0:
+            self.viva = False
